a7b5ba656b3cd06ef29bb1ba0f3180e1442c8b02
laiba-shahid <laiba.shahid@careem.com>
2022-08-29 17:10:11 +0500

    capacity screen added

---
+++ Screens/CapacityScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const CapacityScreen = () => {
+    return (
+        <View>
+            <Text>This is WhereTo Screens</Text>
+        </View>
+    );
+};
+
+export default CapacityScreen;
