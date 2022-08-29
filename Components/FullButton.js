87a6a7e69c4a090cb7412b96785e11ca55778574
laiba-shahid <laiba.shahid@careem.com>
2022-08-29 17:01:16 +0500

    chalo screen changed

---
+++ Components/FullButton.js
@@ -0,0 +1,34 @@
+import { Button } from "@rneui/base";
+import { TouchableOpacity } from "react-native";
+export const FullButton = ({
+    buttonAction,
+    title,
+    buttonStyle = {},
+    disabled = false,
+}) => {
+    return (
+        <TouchableOpacity style={{ width: "90%", alignItems: "center" }}>
+            <Button
+                title={title}
+                size="lg"
+                buttonStyle={{
+                    backgroundColor: "#28bc4e",
+                    borderRadius: 12,
+                    ...buttonStyle,
+                }}
+                containerStyle={{
+                    width: "100%",
+                    marginHorizontal: 50,
+                    marginVertical: 10,
+                }}
+                disabledStyle={{
+                    backgroundColor: "#ADADAD",
+                }}
+                disabledTitleStyle={{ color: "white" }}
+                titleStyle={{ fontWeight: "bold" }}
+                onPress={buttonAction}
+                disabled={disabled}
+            />
+        </TouchableOpacity>
+    );
+};
--- Screens/ChaloScreen.js
+++ Screens/ChaloScreen.js
@@ -1,14 +1,19 @@
 
 
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-
+import { FullButton } from '../Components/FullButton';
 const imageName = require('../assets/chalo.jpg');
 const ChaloScreen = () => {
 
 
     return <View style={styles.container}>
         <ImageBackground source={imageName} resizeMode="cover" style={styles.image}>
-
+            <View style={styles.bottomTab}>
+                <View style={styles.buttonContainer}>
+                    <FullButton title="CHALO" buttonAction={() => { console.log('normal ride') }} />
+                    <FullButton title="SHARE KARO" buttonAction={() => { console.log('decide capacity page') }} />
+                </View>
+            </View>
         </ImageBackground>
     </View>
 };
@@ -29,6 +34,25 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         backgroundColor: '#000000c0',
     },
+    bottomTab: {
+        minWidth: '100%',
+        position: 'absolute',
+        bottom: 0,
+        minHeight: 160,
+        borderRadius: 50,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+
+
+    },
+    buttonContainer: {
+
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+
+
 });
 
 export default ChaloScreen
