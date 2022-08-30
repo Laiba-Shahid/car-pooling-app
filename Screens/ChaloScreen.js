012e6ce67f23a15dddd22cf17a3f9bf69c9e524c
Syed Asad Abrar <syed.abrar@careem.com>
2022-08-30 15:05:11 +0500

    Merged code

--- Screens/ChaloScreen.js
+++ Screens/ChaloScreen.js
@@ -1,43 +1,43 @@
 import { useNavigation } from "@react-navigation/native";
 import {
-    Alert,
-    ImageBackground,
-    Modal,
-    StyleSheet,
-    Text,
-    View,
+  Alert,
+  ImageBackground,
+  Modal,
+  StyleSheet,
+  Text,
+  View,
 } from "react-native";
 import { FullButton } from "../Components/FullButton";
 
 const imageName = require("../assets/chalo.jpg");
 
 const ChaloScreen = ({ navigation }) => {
-    const showSuggestedRidesBtn = false;
-    const navigator = useNavigation("Capacity")
-    return (
-        <View style={styles.container}>
-            <ImageBackground
-                source={imageName}
-                resizeMode="cover"
-                style={styles.image}
-            >
-                <View style={styles.bottomTab}>
-                    <View style={styles.buttonContainer}>
-                        <View style={styles.chaloButtonContainer}>
-                            <FullButton
-                                title="CHALO"
-                                buttonAction={() => {
-                                    console.log("normal ride");
-                                    navigator.navigate("Suggested Rides", {});
-                                }}
-                                buttonStyle={{
-                                    ...(showSuggestedRidesBtn && {
-                                        borderTopRightRadius: 0,
-                                        borderBottomRightRadius: 0,
-                                    }),
-                                }}
-                            />
-                            {/* <FullButton
+  const showSuggestedRidesBtn = false;
+  const navigator = useNavigation("Capacity");
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={imageName}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <View style={styles.bottomTab}>
+          <View style={styles.buttonContainer}>
+            <View style={styles.chaloButtonContainer}>
+              <FullButton
+                title="CHALO"
+                buttonAction={() => {
+                  console.log("normal ride");
+                  navigator.navigate("Suggested Rides", {});
+                }}
+                buttonStyle={{
+                  ...(showSuggestedRidesBtn && {
+                    borderTopRightRadius: 0,
+                    borderBottomRightRadius: 0,
+                  }),
+                }}
+              />
+              {/* <FullButton
                 title="CHALO"
                 buttonAction={() => {
                   console.log("normal ride");
@@ -50,58 +50,58 @@ const ChaloScreen = ({ navigation }) => {
                   }),
                 }}
               /> */}
-                        </View>
-                        <FullButton
-                            title="SHARE KARO"
-                            buttonAction={() => {
-                                navigator.navigate("Capacity")
-                            }}
-                        />
-                    </View>
-                </View>
-            </ImageBackground>
+            </View>
+            <FullButton
+              title="SHARE KARO"
+              buttonAction={() => {
+                navigator.navigate("Capacity");
+              }}
+            />
+          </View>
         </View>
-    );
+      </ImageBackground>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    image: {
-        flex: 1,
-        justifyContent: "center",
-    },
-    modalView: {
-        flexDirection: "row",
-        backgroundColor: "red",
-    },
-    text: {
-        color: "white",
-        fontSize: 42,
-        lineHeight: 84,
-        fontWeight: "bold",
-        textAlign: "center",
-        backgroundColor: "#000000c0",
-    },
-    bottomTab: {
-        minWidth: "100%",
-        position: "absolute",
-        bottom: 0,
-        minHeight: 160,
-        borderRadius: 50,
-        backgroundColor: "#ffffff",
-        justifyContent: "center",
-    },
-    buttonContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    chaloButtonContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
-    },
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  modalView: {
+    flexDirection: "row",
+    backgroundColor: "red",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
+  },
+  bottomTab: {
+    minWidth: "100%",
+    position: "absolute",
+    bottom: 0,
+    minHeight: 160,
+    borderRadius: 50,
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  chaloButtonContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
 });
 
 export default ChaloScreen;
